bb9920d38ea6c52969cb869db625ca2d06d7be34
yuna0831 <yshin0831@gmail.com>
2025-09-13 19:44:57 -0500

    Added ImageView to MainActivity

--- app/src/main/java/com/cs407/helloworld/MainActivity.kt
+++ app/src/main/java/com/cs407/helloworld/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.cs407.helloworld.ui.theme.HelloWorldTheme
 
@@ -20,10 +23,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // Column: Text + Image 같이 배치
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Greeting(name = "Android")
+                        // ImageView 역할
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                            contentDescription = "App Icon"
+                        )
+                    }
                 }
             }
         }
@@ -42,6 +50,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HelloWorldTheme {
-        Greeting("Android")
+        Column {
+            Greeting("Android")
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "App Icon"
+            )
+        }
     }
 }
